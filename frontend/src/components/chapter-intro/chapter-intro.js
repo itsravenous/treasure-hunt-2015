60d876cf599ae5355ee2418036ecfd7d5a78186b
Tom Jenkins <tom@itsravenous.com>
2015-11-29 23:59:09 +0000

    Animate chapter intros in

--- frontend/src/components/chapter-intro/chapter-intro.js
+++ frontend/src/components/chapter-intro/chapter-intro.js
@@ -1,11 +1,26 @@
 var React = require('react');
 
 var ChapterIntro = React.createClass({
+	getInitialState: function () {
+		return {
+			ready: false
+		};
+	},
+
+	componentDidMount: function() {
+		setTimeout(function () {
+			this.setState({
+				ready: true
+			});
+		}.bind(this), 100);
+	},
+
 	render: function () {
 		var bgStyle = {
 			backgroundImage: 'url("'+this.props.image+'")'
 		};
-		return <div className="chapter-intro" style={bgStyle}>
+		return <div data-state={this.state.ready ? 'ready' : 'unready'} className="chapter-intro">
+			<div className="bg" style={bgStyle} />
 			<h1>
 				<span className="number">{this.props.chapter}.</span>
 				{this.props.title}
