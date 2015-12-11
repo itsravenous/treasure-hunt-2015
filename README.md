d6fc25a2f6b10532520be45a97a3c1a67d660f11
Tom Jenkins <tom@itsravenous.com>
2015-12-11 20:35:29 +0000

    Add lightswitch icon and dialogue @todo store images in component folder @todo prevent swipe refresh on chrome

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 [Studies of rowing boats and a very small view of a sailing vessel](http://collections.rmg.co.uk/collections/objects/129256.html) (National Maritime Museum, Greenwich, London) - cropped and vignetted to show single vessel
 ### Chapter 4
 [Skulls: above, diagrams demonstrating the proportions of the human skull; below, four views of the skull. Line engraving.](http://wellcomeimages.org/indexplus/image/V0007918.html) (Wellcome Library, London) - cropped and vignetted to show single skull
+[Lightswitch](https://thenounproject.com/term/light-switch/2235/) (The Noun Project)
--- frontend/src/components/prayer/prayer.js
+++ frontend/src/components/prayer/prayer.js
@@ -2,16 +2,74 @@ var React = require('react');
 var Link = require('react-router').Link;
 var chapters = require('../../data/chapters');
 var switchClient = require('../../js/switch-client');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 var switches = switchClient.getSwitches();
+var MESSAGES = [
+	'I see that you have come to pray.',
+	'You must know:',
+	'True humility requires true darkness.',
+	false, // Signifies break for lightswitch
+	'With total darkness comes total clarity.',
+	'Seek a lone star',
+	'He is waiting for you there'
+];
+var MESSAGES1 = 0;
+var SWITCH_ON = 1;
+var SWITCH_OFF = 2;
+var MESSAGES2 = 3;
 
 var Prayer = React.createClass({
 
 	getInitialState: function () {
-		return {};
+		return {
+			stage: MESSAGES1,
+			message: -1,
+		};
+	},
+
+	componentDidMount: function () {
+		this.startDialogue();
+	},
+
+	startDialogue: function () {
+		setTimeout(this.nextDialogue, 100);
+		this.interval = setInterval(this.nextDialogue, 6000);
+	},
+
+	nextDialogue: function () {
+		var nextMessage = MESSAGES[this.state.message + 1];
+		if (nextMessage) {
+			this.setState({
+				message: this.state.message + 1
+			});
+		} else if (this.state.stage !== MESSAGES2) {
+			this.setState({
+				message: this.state.message + 1
+			});
+			this.showSwitch();
+			clearInterval(this.interval);
+		} else {
+			clearInterval(this.interval);
+		}
+	},
+
+	showSwitch: function () {
+		this.setState({
+			stage: SWITCH_ON
+		});
 	},
 
 	lightsOut: function () {
+		this.setState({
+			stage: SWITCH_OFF
+		});
+		setTimeout(function () {
+			this.setState({
+				stage: MESSAGES2
+			});
+			this.startDialogue();
+		}.bind(this), 1500);
 		switches.then(function (switches) {
 			// Dumbly flick first switch @todo check status first, make switch number configurable
 			switchClient.off(switches[0].id);
@@ -22,9 +80,18 @@ var Prayer = React.createClass({
 	},
 
 	render: function () {
+		var display = null;
+		if (this.state.stage === SWITCH_ON) {
+			display = <button onClick={this.lightsOut} className="lightswitch" data-state="on">Turn out the light</button>;
+		} else if (this.state.stage === SWITCH_OFF){
+			display = <button onClick={this.lightsOut} className="lightswitch" data-state="off">Turn out the light</button>;
+		} else if (this.state.message > -1 && (this.state.stage === MESSAGES1 || this.state.stage === MESSAGES2)) {
+			display = <p className="dialogue" key={new Date().getTime()}>{MESSAGES[this.state.message]}</p>;
+		}
 		return <div className="prayer">
-			<h1>True humility requires darkness.</h1>
-			<button onClick={this.lightsOut}>Turn out the light</button>
+			<ReactCSSTransitionGroup transitionName="dialogue" transitionEnterTimeout={1500} transitionLeaveTimeout={1500}>
+				{display}
+			</ReactCSSTransitionGroup>
 		</div>;
 	}
 });
